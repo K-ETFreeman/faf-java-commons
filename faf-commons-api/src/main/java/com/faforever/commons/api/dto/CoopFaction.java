ecc3dea25837a519b3ab1a3af6d7c7ed076a0cd3
Marc-Spector <38242291+Marc-Spector@users.noreply.github.com>
2022-12-18 17:40:51 +0300

    Add CoopScenario entity (#99)
    Co-authored-by: Ivan <v23620@gmail.com>

---
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopFaction.java
@@ -0,0 +1,5 @@
+package com.faforever.commons.api.dto;
+
+public enum CoopFaction {
+  UEF, CYBRAN, AEON, SERAPHIM, CUSTOM
+}
--- faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopScenario.java
+++ faf-commons-api/src/main/java/com/faforever/commons/api/dto/CoopScenario.java
@@ -2,29 +2,31 @@ package com.faforever.commons.api.dto;
 
 import com.faforever.commons.api.elide.ElideEntity;
 import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Relationship;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import java.util.List;
+
 @Data
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Type("coopMission")
-public class CoopMission implements ElideEntity {
+@Type("coopScenario")
+public class CoopScenario implements ElideEntity {
+
   @Id
   @ToString.Include
   @EqualsAndHashCode.Include
   private String id;
   @ToString.Include
   private String name;
+  private String description;
   private int order;
   @ToString.Include
-  private int version;
-  private String category;
-  private String thumbnailUrlSmall;
-  private String thumbnailUrlLarge;
-  private String description;
-  private String downloadUrl;
-  private String folderName;
+  private CoopType type;
+  private CoopFaction faction;
+  @Relationship("maps")
+  private List<CoopMission> maps;
 }
