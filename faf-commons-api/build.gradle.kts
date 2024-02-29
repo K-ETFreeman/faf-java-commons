99de5990549b9e30cc94e5b11eab9d55d56099f8
Brutus5000 <Brutus5000@gmx.net>
2024-02-29 21:59:29 +0100

    Migrate subprojects to Gradle Kotlin syntax

--- faf-commons-api/build.gradle.kts
+++ faf-commons-api/build.gradle.kts
@@ -1,7 +1,7 @@
-version "1.0-SNAPSHOT"
+version = "1.0-SNAPSHOT"
 
 dependencies {
-  implementation project(":faf-commons-data")
+  implementation(project(":faf-commons-data"))
   implementation(libs.jsonapi.converter)
   implementation(libs.q.builders)
 
