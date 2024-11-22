e4f035e407de6239455cc7552d09da391f6ee097
Brutus5000 <Brutus5000@gmx.net>
2024-11-22 22:32:43 +0100

    Allow releasing to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,5 +25,16 @@ configure(subprojects) {
         from(components["java"])
       }
     }
+
+    repositories {
+      maven {
+        name = "GitHubPackages"
+        setUrl("https://maven.pkg.github.com/FAForever/faf-java-commons")
+        credentials {
+          username = System.getenv("GITHUB_ACTOR")
+          password = System.getenv("GITHUB_TOKEN")
+        }
+      }
+    }
   }
 }
