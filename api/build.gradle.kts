232064ec59d2dda40c0e48b617ba2a44eab202ed
Sheikah45 <Sheikah450@gmail.com>
2024-11-23 18:18:05 -0500

    Publish to Maven central

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+  id("com.vanniktech.maven.publish") version "0.33.0"
+}
+
 java {
   sourceCompatibility = JavaVersion.VERSION_21
   targetCompatibility = JavaVersion.VERSION_21
@@ -22,4 +26,40 @@ dependencies {
   testImplementation(libs.mockito.junit.jupiter)
   testImplementation(libs.pojo.tester)
   testCompileOnly(libs.jetbrains.annotations)
+}
+
+mavenPublishing {
+  publishToMavenCentral()
+  signAllPublications()
+
+  coordinates("com.faforever.commons", "api", project.version.toString())
+  // Configure POM metadata
+  pom {
+    name.set("api")
+    description.set("API DTOs for FAForever Java API")
+    url.set("https://github.com/FAForever/faf-java-commons")
+    licenses {
+      license {
+        name.set("MIT")
+        url.set("https://www.opensource.org/licenses/mit-license.php")
+      }
+    }
+    developers {
+      developer {
+        id.set("Brutus5000")
+        name.set("Brutus5000")
+        organization.set("FAForever")
+        organizationUrl.set("https://github.com/FAForever")
+      }
+    }
+    scm {
+      url.set("https://github.com/FAForever/faf-java-commons")
+      connection.set("scm:git:https://github.com/FAForever/faf-java-commons")
+      developerConnection.set("scm:git:https://github.com/FAForever/faf-java-commons")
+    }
+    issueManagement {
+      system.set("GitHub")
+      url.set("https://github.com/FAForever/faf-java-commons/issues")
+    }
+  }
 }
